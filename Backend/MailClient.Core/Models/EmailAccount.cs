9a74cd695283e82060a05db668c446fc0c7d0bb7
Nguyen <huunguyen.personal@gmail.com>
2025-10-25 09:31:36 +0700

    Add models

---
+++ Backend/MailClient.Core/Models/EmailAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailClient.Core.Models
+{
+    public class EmailAccount
+    {
+        public String Address { get; set; } = String.Empty;
+        public String DisplayName { get; set; } = String.Empty;
+        public String SmtpServer { get; set; } = String.Empty;
+        public int SmtpPort { get; set; } = 587; // for STARTTLS
+        public String ImapServer { get; set; } = String.Empty;
+        public int ImapPort { get; set; } = 993;
+        public String Password {  get; set; } = String.Empty;
+    }
+}
---
+++ Backend/MailClient.Core/Models/MailMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailClient.Core.Models
+{
+    public class MailMessage
+    {
+        public String Id { get; set; } = String.Empty;
+        public String From { get; set; } = String.Empty;
+        public List<String> To { get; set; } = new List<String>();
+        public String Subject { get; set; } = String.Empty;
+        public String Body { get; set; } = String.Empty;
+        public DateTime Date {  get; set; } = DateTime.Now;
+        public bool IsRead { get; set; } = false;
+    }
+}
