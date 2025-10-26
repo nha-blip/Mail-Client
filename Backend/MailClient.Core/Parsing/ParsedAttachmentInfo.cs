1ae0f6edf45c7148de1730d54c8cbdf15456fe3f
PeterBrr <dinhquangnhat16062006@gmail.com>
2025-10-26 11:01:58 +0700

    Thêm project MailClient.Core.Parsing

---
+++ Backend/MailClient.Core/Parsing/ParsedAttachmentInfo.cs
@@ -0,0 +1,16 @@
+﻿using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailClient.Core.Parsing
+{
+    public class ParsedAttachmentInfo
+    {
+        public string FileName { get; set; }
+        public long SizeInBytes { get; set; }
+        internal MimePart OriginalMimePart { get; set; }
+    }
+}
