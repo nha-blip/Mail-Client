34d7f62072016a26588e498ebd830c6f918f155d
Trong Nguyen <buitrantrongnguyen@gmail.com>
2025-11-14 00:59:15 +0700

    Thêm file mới vào Mailclient_UI_UX

---
+++ UI_UX/Mailclient_UI_UX/Register.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Mailclient
+{
+    /// <summary>
+    /// Interaction logic for Register.xaml
+    /// </summary>
+    public partial class Register : UserControl
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void oplogin(object sender, MouseButtonEventArgs e)
+        {
+            Login? parentWindow = Window.GetWindow(this) as Login;
+
+            // 2. Gọi hàm "ShowLoginView()" của cha
+            if (parentWindow != null)
+            {
+                parentWindow.ShowLoginView();
+            }
+        }
+    }
+}
